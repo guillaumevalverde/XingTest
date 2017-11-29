4ea1adff39e5793f387c90d7bfa5040b5c19352e
valverde Guillaume <guillaume.valverde@gmail.com>
2017-11-29 13:06:20 +0100

    add dialog on Long click with implicit intent to open url in browser

--- app/src/main/java/com/gve/testapplication/ListOfRepoFeature/presentation/RepositoryViewHolder.java
+++ app/src/main/java/com/gve/testapplication/ListOfRepoFeature/presentation/RepositoryViewHolder.java
@@ -1,13 +1,20 @@
 package com.gve.testapplication.ListOfRepoFeature.presentation;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.support.annotation.NonNull;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.gve.testapplication.ListOfRepoFeature.data.Repository;
 import com.gve.testapplication.R;
@@ -17,6 +24,8 @@ import com.gve.testapplication.core.recyclerview.ViewHolderBinder;
 import com.gve.testapplication.core.recyclerview.ViewHolderFactory;
 
 import javax.inject.Inject;
+
+import static com.gve.testapplication.core.utils.IntentUtils.isAvailable;
 
 public class RepositoryViewHolder extends RecyclerView.ViewHolder {
 
@@ -38,10 +47,55 @@ public class RepositoryViewHolder extends RecyclerView.ViewHolder {
                 .getString(R.string.repo_ddescritpion, repository.getDescription()));
         repoOwnerLoginTv.setText(repoOwnerLoginTv.getResources()
                 .getString(R.string.repo_owner_login, repository.getLoginOwner()));
-        itemView.setBackgroundColor(repository.getFork()
-                ? itemView.getContext().getResources().getColor(R.color.background_card_fork_true)
-                : itemView.getContext().getResources().getColor(R.color.background_card_fork_false));
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            itemView.setBackground(repository.getFork()
+                    ? itemView.getContext().getResources()
+                    .getDrawable(R.drawable.ripple_effect_fork_true)
+                    : itemView.getContext().getResources()
+                    .getDrawable(R.drawable.ripple_effect_fork_false));
+        } else {
+            itemView.setBackgroundColor(repository.getFork()
+                    ? itemView.getContext().getResources()
+                    .getColor(R.color.background_card_fork_true)
+                    : itemView.getContext().getResources()
+                    .getColor(R.color.background_card_fork_false));
+        }
+
+        itemView.setOnLongClickListener(view -> showDialog(repository));
+    }
+
+    private Boolean showDialog(Repository repository) {
+        final Dialog dialog = new Dialog(itemView.getContext());
+        dialog.setCancelable(true);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_repo);
+
+        Button ownerButton =  dialog.findViewById(R.id.dialog_owner_button);
+        Button repoButton =  dialog.findViewById(R.id.dialog_repo_button);
+
+        ownerButton.setOnClickListener(v -> {
+            launchBrowserWithUrl(repository.getOwnerUrl());
+            dialog.dismiss();
+        });
+
+        repoButton.setOnClickListener(v -> {
+            launchBrowserWithUrl(repository.getRepoUrl());
+            dialog.dismiss();
+        });
+        dialog.show();
+        return true;
+    }
+
+    private void launchBrowserWithUrl(String url) {
+        Log.v("dialog", " launch browser with: " + url);
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        if (isAvailable(itemView.getContext(), browserIntent)) {
+            itemView.getContext().startActivity(browserIntent);
+        } else {
+            Toast.makeText(itemView.getContext(), itemView.getResources()
+                    .getString(R.string.dialog_no_browser), Toast.LENGTH_LONG).show();
+        }
     }
 
     public static class RepoCardHolderFactory extends ViewHolderFactory {
---
+++ app/src/main/java/com/gve/testapplication/core/utils/IntentUtils.java
@@ -0,0 +1,22 @@
+package com.gve.testapplication.core.utils;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+
+import java.util.List;
+
+/**
+ * Created by gve on 29/11/2017.
+ */
+
+public class IntentUtils {
+    public static boolean isAvailable(Context ctx, Intent intent) {
+        final PackageManager mgr = ctx.getPackageManager();
+        List<ResolveInfo> list =
+                mgr.queryIntentActivities(intent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
+    }
+}
